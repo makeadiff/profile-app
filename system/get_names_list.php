567f00217754e9a7536f7d08ff77ac704bccbd61
Sanjay Thomas Alanoly <sanjay.thomas@yahoo.com>
2013-11-28 20:55:06 +0530

    HR Dashboard

--- system/get_names_list.php
+++ system/get_names_list.php
@@ -11,9 +11,9 @@ if($people) {
 print "<table>";
 print"<th>Name</th><th>Phone</th><th>Progress %</th>";
 foreach($people as $person) {
-	print "<tr><td>$person['name']</td><td>$person['phone']</td><td>$person['profile_progress']</td></tr>";
+	print "<tr><td>$person[name]</td><td>$person[phone]</td><td>$person[profile_progress]</td></tr>";
 	$counter++;
-	$city_progress += $person['profile_progress']; 
+	$city_progress = $city_progress + $person['profile_progress']; 
 }
 
 $city_progress = $city_progress/$counter;
