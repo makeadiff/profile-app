9c2ea4deba22b355710c6f9b4688e50f3a1500e7
Sanjay Thomas Alanoly <sanjay.thomas@yahoo.com>
2013-11-28 10:16:53 +0530

    HR Dashboard, hr.js

--- js/hr.js
+++ js/hr.js
@@ -3,29 +3,24 @@ function init() {
 	
 	$("#city_id").change(function() {
 		if(this.value != "0") {
-			$("#your_name").prop("disabled", false);
-		} else {
-			$("#your_name").prop("disabled", true);
 			
-		}
+			var city_id = $("#city_id").val();
+			
+			
+			$.ajax("system/get_names_list.php?city_id="+city_id, {
+				"success": function(data) {
+					$("#people_area").html(data);
+				},
+				error: function() {
+					alert("System Error: Can't get names. Please try later");
+				}
+				
+				
+			});
+			
+		} 
 	});
-	
-	
-	$("#search").click(function() {
-		var city_id = $("#city_id").val();
-		
-		
-		$.ajax("system/get_names_list.php?city_id="+city_id, {
-			"success": function(data) {
-				$("#people_area").html(data);
-			},
-			error: function() {
-				alert("System Error: Can't get names. Please try later");
-			}
-			
-			
-		});
-	});
+
 	
 }
 
