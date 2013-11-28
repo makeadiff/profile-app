9b807b760500a6e51bf28312c4c106f2e054bc66
Sanjay Thomas Alanoly <sanjay.thomas@yahoo.com>
2013-11-28 10:36:39 +0530

    get_names_list.php

---
+++ system/get_names_list.php
@@ -0,0 +1,18 @@
+<?php
+require('../../common.php');
+
+$city_id = intval($_REQUEST['city_id']);
+
+$people = $sql->getById("SELECT id, name,phone,profile_progress FROM User WHERE status='1' AND user_type='volunteer' AND city_id=$city_id");
+
+if($people) {
+print "<table>";
+foreach($people as $person) {
+	print "<tr><td>$person->name</td><td>$person->phone</td><td>$person->profile_progress</td></tr>";
+}
+
+print "</table>";
+
+}
+
+?> 
