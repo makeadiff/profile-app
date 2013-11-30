f48129e396ced545291d60e29d460181b7881dd9
Binny V A <binnyva@gmail.com>
2013-12-01 00:55:20 +0530

    Fixed bug with verification check

--- common.php
+++ common.php
@@ -4,6 +4,6 @@ require '../common.php';
 function isVerified($type, $user_id) {
 	global $sql;
 	$verification_status = json_decode($sql->getOne("SELECT verification_status FROM User WHERE id=$user_id"));
-	
+	if(!$verification_status) $verification_status = array();
 	return in_array($type, $verification_status);
 }
