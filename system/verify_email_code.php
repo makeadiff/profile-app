69a84ec0e6c6455d4a695570fefc6198012a0312
Binny V A <binnyva@gmail.com>
2013-12-01 16:02:37 +0530

    Fixed bug with verification.

--- system/verify_email_code.php
+++ system/verify_email_code.php
@@ -10,6 +10,7 @@ $code = substr(md5($user_id . '|' . $email),0,5);
 if($code == $email_code) {
 	if(!isVerified('email', $user_id)) {
 		$verification_status = json_decode($sql->getOne("SELECT verification_status FROM User WHERE id=$user_id"));
+		if(!$verification_status) $verification_status = array();
 		array_push($verification_status, 'email');
 		
 		$sql->update('User', array('verification_status' => json_encode($verification_status)), "id=$user_id");
--- system/verify_sms_code.php
+++ system/verify_sms_code.php
@@ -10,6 +10,7 @@ $code = substr(md5($user_id . "|" . $phone),0,5);
 if($code == $phone_code) {
 	if(!isVerified('sms', $user_id)) {
 		$verification_status = json_decode($sql->getOne("SELECT verification_status FROM User WHERE id=$user_id"));
+		if(!$verification_status) $verification_status = array();
 		array_push($verification_status, 'sms');
 		
 		$sql->update('User', array('verification_status' => json_encode($verification_status)), "id=$user_id");
