a1028fe17b194f37b396024e9c5cc8010994f8a3
Binny V A <binnyva@gmail.com>
2016-07-20 17:31:39 +0530

    Auto find your porfile from madapp login

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 require './common.php';
+if(empty($_SESSION['user_id'])) {
+	$url_parts = parse_url($config['site_url']);
+	$domain = $url_parts['scheme'] . '://' . $url_parts['host'];
+	$madapp_url = "http://makeadiff.in/madapp/";
+	if(strpos($config['site_home'], 'localhost') !== false) $madapp_url = "http://localhost/Projects/Madapp/";
+
+	header("Location: " . $madapp_url . "index.php/auth/login/" . base64_encode($domain . $config['PHP_SELF']));
+	exit;
+}
+
+$user_id = $_SESSION['user_id'];
+$current_user = $sql->from('User')->find($user_id);
+header("Location: fb.php?user_id=$user_id");
+exit;
+
+
+////////////// Everything below this is old. WILL NOT BE CALLED
+
 
 $cities = $sql->getById("SELECT id,name FROM City WHERE type='actual' ORDER BY name");
 ?><!DOCTYPE HTML>
